4a3807cc844fa484939d9c96beeeec1a909fe363
Mak Kader <abdul.kader880@gmail.com>
2026-02-14 19:42:39 -0600

    npm install on extension at startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,12 @@ set -e
 
 Xvfb :99 -screen 0 1920x1080x24 &
 
+# Run npm install in all extension directories with package.json
+if [ -d "/app/.pi/extensions" ]; then
+    find /app/.pi/extensions -type f -name "package.json" -exec dirname {} \; | while read -r dir; do
+        echo "Installing npm dependencies in: $dir"
+        (cd "$dir" && npm install)
+    done
+fi
+
 exec "$@"
